cb6a63498a8ed77924a22aea3602246b4b45963b
Pavel Panin <pavel99499@gmail.com>
2019-07-26 17:45:22 +0300

    fix del

--- src/pages/Houses/rest.ts
+++ src/pages/Houses/rest.ts
@@ -6,5 +6,5 @@ export default {
   getItem: (id: any) => http.get(`${apiUrl}?id=${id}`),
   createItem: (item: any) => http.put(apiUrl, item),
   saveItem: (item: any) => http.patch(apiUrl, item),
-  removeItem: (item: any) => http.patch(apiUrl, { id: item.id })
+  removeItem: (item: any) => http.delete(apiUrl, { data: { id: item.id } })
 }
