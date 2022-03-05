afbcfe8df96006b43ebbdd7016253e7b9a0ff7f4
Pablo <53233389+pablitopanini@users.noreply.github.com>
2022-03-05 11:03:08 +0300

    test vscode dev

--- src/index.tsx
+++ src/index.tsx
@@ -1,37 +1,37 @@
-import * as React from 'react'
-import createSagaMiddleware from 'redux-saga'
-import Routes from './Routes'
-import rootReducer from './reducer'
-import sagas from './sagas'
-import { applyMiddleware, createStore, Store } from 'redux'
-import { BrowserRouter } from 'react-router-dom'
-import { composeWithDevTools } from 'redux-devtools-extension'
-import { IStore } from './interfaces'
-import { Provider } from 'react-redux'
-import { render } from 'react-dom'
-import 'react-app-polyfill/ie11'
-import { LocaleProvider, Layout } from 'antd'
-import Login from './pages/Login'
-import ruRU from 'antd/lib/locale-provider/ru_RU'
-import './style.css'
-import Menu from './pages/Menu'
+import * as React from "react";
+import createSagaMiddleware from "redux-saga";
+import Routes from "./Routes";
+import rootReducer from "./reducer";
+import sagas from "./sagas";
+import { applyMiddleware, createStore, Store } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { IStore } from "./interfaces";
+import { Provider } from "react-redux";
+import { render } from "react-dom";
+import "react-app-polyfill/ie11";
+import { LocaleProvider, Layout } from "antd";
+import Login from "./pages/Login";
+import ruRU from "antd/lib/locale-provider/ru_RU";
+import "./style.css";
+import Menu from "./pages/Menu";
 
-const composeEnhancers = composeWithDevTools({})
+const composeEnhancers = composeWithDevTools({});
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   {},
   composeEnhancers(applyMiddleware(sagaMiddleware))
-) as Store<IStore>
+) as Store<IStore>;
 
-sagas.forEach(saga => {
+sagas.forEach((saga) => {
   // tslint:disable-next-line: forin
   for (const name in saga) {
-    sagaMiddleware.run(saga[name])
+    sagaMiddleware.run(saga[name]);
   }
-})
+});
 
 render(
   <Provider store={store}>
@@ -49,9 +49,9 @@ render(
 
             <Layout.Content
               style={{
-                height: 'calc(100vh - 133px)',
-                backgroundColor: 'white',
-                borderBottom: '1px solid #e8e8e8'
+                height: "calc(100vh - 133px)",
+                backgroundColor: "white",
+                borderBottom: "1px solid #e8e8e8",
               }}
             >
               <Routes />
@@ -59,13 +59,13 @@ render(
           </Layout>
 
           <Layout.Footer
-            style={{ textAlign: 'center', backgroundColor: 'white' }}
+            style={{ textAlign: "center", backgroundColor: "white" }}
           >
-            {`🐕& 🦊©${new Date().getFullYear()}`}
+            {`🐕 & 🦊©${new Date().getFullYear()}`}
           </Layout.Footer>
         </Layout>
       </LocaleProvider>
     </BrowserRouter>
   </Provider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
